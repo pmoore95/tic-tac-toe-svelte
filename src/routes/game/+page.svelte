3797e61d34de663d95ae671cb051dfce3cb83603
pmoore95 <57687000+pmoore95@users.noreply.github.com>
2024-01-10 02:47:57 +0000

    fix players board

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -4,13 +4,12 @@
 	import '../../app.css';
 	import Board from './components/board.svelte';
 	import playerMarker from '$lib/stores/playerMarker';
-	import cpuMarker from '$lib/stores/cpuMarker';
 	import gameResult from '$lib/stores/gameResult';
 	import { GameResult, type GameResultType } from '$lib/constants';
 	import GameResultModal, {
 		type GameResultModalProps
 	} from '$lib/components/GameResultModal.svelte';
-	import Marker from '$lib/components/Marker.svelte';
+	import Players from './components/players.svelte';
 
 	if (browser && !$playerMarker) {
 		goto('/');
@@ -35,20 +34,7 @@
 		<h1 class="font-dela-gothic text-white mb-8 text-3xl text-center">Tic Tac Toe</h1>
 		<Board />
 	</div>
-	<div class="ml-8 grid grid-cols-2 gap-4">
-		<div class="flex items-center">
-			<h2 class="font-dela-gothic text-white text-2xl mr-4 text-right w-full">You:</h2>
-		</div>
-		<div class="w-[50px] h-[50px] bg-red-500 rounded-full p-2">
-			<Marker type={$playerMarker} className="w-full !h-full" />
-		</div>
-		<div class="flex items-center">
-			<h2 class="font-dela-gothic text-white text-2xl mr-4 text-right w-full">CPU:</h2>
-		</div>
-		<div class="w-[50px] h-[50px] bg-blue-500 rounded-full p-2">
-			<Marker type={$cpuMarker} className="w-full !h-full" />
-		</div>
-	</div>
+	<Players/>
 </div>
 
 {#if $gameResult}
---
+++ src/routes/game/components/players.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Marker from "$lib/components/Marker.svelte";
+    import playerMarker from "$lib/stores/playerMarker";
+    import cpuMarker from "$lib/stores/cpuMarker";
+	import { MarkerEnum } from "$lib/constants";
+
+    const BACKGROUND_COLOR = {
+        [MarkerEnum.CIRCLE]: 'bg-red-500',
+        [MarkerEnum.CROSS]: 'bg-blue-500'
+    }
+
+</script>
+
+
+<div class="ml-8 grid grid-cols-2 gap-4">
+    <div class="flex items-center">
+        <h2 class="font-dela-gothic text-white text-2xl mr-4 text-right w-full">You:</h2>
+    </div>
+    <div class={`w-[50px] h-[50px] rounded-full p-2 ${BACKGROUND_COLOR[$playerMarker]}`}>
+        <Marker type={$playerMarker} className="w-full !h-full" />
+    </div>
+    <div class="flex items-center">
+        <h2 class="font-dela-gothic text-white text-2xl mr-4 text-right w-full">CPU:</h2>
+    </div>
+    <div class={`w-[50px] h-[50px] bg-blue-500 rounded-full p-2 ${BACKGROUND_COLOR[$cpuMarker]}`}>
+        <Marker type={$cpuMarker} className="w-full !h-full" />
+    </div>
+</div>
